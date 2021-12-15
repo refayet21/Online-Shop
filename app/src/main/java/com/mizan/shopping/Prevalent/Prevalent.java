4a3fee6941d616d5feb7883017f825987f45a22b
refayet21 <refayet21@gmail.com>
2021-12-15 12:01:28 +0600

    first commit

---
+++ app/src/main/java/com/mizan/shopping/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.mizan.shopping.Prevalent;
+
+import com.mizan.shopping.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
